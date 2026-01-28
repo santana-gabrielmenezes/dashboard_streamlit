482d7b3858808804744cb01c02e435a1bdeef265
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2026-01-28 05:29:48 -0300

    inseridos título e dataframe das vendas no arquivo 'Dashboard.py'

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,15 @@
-# instalando bibliotecas
+# bibliotecas
 import streamlit as st
 import requests
 import pandas as pd
 import plotly.express as px
 
+# título do dashboard
+st.title('DASHBOARD DE VENDAS :shopping_cart:')
+
+# dataframe de vendas
+url = 'https://labdados.com/produtos'
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+
+st.dataframe(dados)
